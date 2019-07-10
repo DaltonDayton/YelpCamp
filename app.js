e192dbb06544423d11d236bae704f40ca97a54c5
Dalton Dayton <daltondayton1@gmail.com>
2019-07-10 15:20:26 -0600

    Add styling

--- app.js
+++ app.js
@@ -20,6 +20,21 @@ var campgrounds = [
         name: "Mountain Goat's Rest",
         image:
             "https://static.rootsrated.com/image/upload/s--_i1nqUT1--/t_rr_large_traditional/oy9xsbijv8wehnrzv0zt.jpg"
+    },
+    {
+        name: "Salmon Creek",
+        image:
+            "https://www.nps.gov/shen/planyourvisit/images/20170712_A7A9022_nl_Campsites_BMCG_960.jpg?maxwidth=1200&maxheight=1200&autorotate=false"
+    },
+    {
+        name: "Granite Hill",
+        image:
+            "https://assets.simpleviewinc.com/simpleview/image/fetch/c_fill,h_452,q_75,w_982/http://res.cloudinary.com/simpleview/image/upload/v1469218578/clients/lanecounty/constitution_grove_campground_by_natalie_inouye_417476ef-05c3-464d-99bd-032bb0ee0bd5.png"
+    },
+    {
+        name: "Mountain Goat's Rest",
+        image:
+            "https://static.rootsrated.com/image/upload/s--_i1nqUT1--/t_rr_large_traditional/oy9xsbijv8wehnrzv0zt.jpg"
     }
 ];
 
