dbded7a69bec1d25639f9f91267cfbdfb1fa5b0c
Dalton Dayton <daltondayton1@gmail.com>
2019-07-22 14:15:51 -0600

    Comment authorization

--- routes/comments.js
+++ routes/comments.js
@@ -46,7 +46,7 @@ router.post("/", isLoggedIn, (req, res) => {
 });
 
 // Comments Edit
-router.get("/:comment_id/edit", (req, res) => {
+router.get("/:comment_id/edit", checkCommentOwnership, (req, res) => {
 	Comment.findById(req.params.comment_id, (err, foundComment) => {
 		if (err) {
 			res.redirect("back");
@@ -60,7 +60,7 @@ router.get("/:comment_id/edit", (req, res) => {
 });
 
 // Comments Update
-router.put("/:comment_id", (req, res) => {
+router.put("/:comment_id", checkCommentOwnership, (req, res) => {
 	Comment.findByIdAndUpdate(
 		req.params.comment_id,
 		req.body.comment,
@@ -75,7 +75,7 @@ router.put("/:comment_id", (req, res) => {
 });
 
 // Comments Destroy
-router.delete("/:comment_id", (req, res) => {
+router.delete("/:comment_id", checkCommentOwnership, (req, res) => {
 	Comment.findByIdAndRemove(req.params.comment_id, err => {
 		if (err) {
 			res.redirect("back");
@@ -86,6 +86,25 @@ router.delete("/:comment_id", (req, res) => {
 });
 
 // Middleware
+function checkCommentOwnership(req, res, next) {
+	if (req.isAuthenticated()) {
+		Comment.findById(req.params.comment_id, (err, foundComment) => {
+			if (err) {
+				res.redirect("back");
+			} else {
+				// Does user own the comment?
+				if (foundComment.author.id.equals(req.user._id)) {
+					next();
+				} else {
+					res.redirect("back");
+				}
+			}
+		});
+	} else {
+		res.redirect("back");
+	}
+}
+
 function isLoggedIn(req, res, next) {
 	if (req.isAuthenticated()) {
 		return next();
